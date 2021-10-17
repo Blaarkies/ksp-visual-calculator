e492b5e46b00394968bde96e2bce27b34a9366f4
Pierre Roux <pierre.roux6@gmail.com>
2021-10-17 12:35:25 +0200

    Fix trip details tooltips appearing in unscrolled positions

--- src/app/components/maneuver-sequence-panel/msp-edge/msp-edge.component.ts
+++ src/app/components/maneuver-sequence-panel/msp-edge/msp-edge.component.ts
@@ -4,6 +4,8 @@ import { animate, group, state, style, transition, trigger } from '@angular/anim
 import { CustomAnimation } from '../../../common/domain/custom-animation';
 import { MissionEdge } from '../maneuver-sequence-panel.component';
 import { PathDetailsReader } from './path-details-reader';
+import { ConnectionPositionPair } from '@angular/cdk/overlay';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'cp-msp-edge',
@@ -11,17 +13,14 @@ import { PathDetailsReader } from './path-details-reader';
   styleUrls: ['./msp-edge.component.scss'],
   animations: [
     trigger('animateSlidePanel', [
-      state('false', style({transform: 'translateX(0) scaleY(0)'})),
-      state('true', style({transform: 'translateX(-100%) scaleY(1)'})),
-      transition('false => true', [
-        group([
-          animate('.3s ease-out', style({transform: 'translateX(-100%) scaleY(1)'})),
-        ]),
+      state('*', style({transform: 'translateX(-100%) scaleY(1)'})),
+      transition(':enter', [
+        style({transform: 'translateX(0) scaleY(0)'}),
+        animate('.3s ease-out', style({transform: 'translateX(-100%) scaleY(1)'})),
       ]),
-      transition('true => false', [
-        group([
-          animate('.3s ease-in', style({transform: 'translateX(0) scaleY(0)'})),
-        ]),
+      transition(':leave', [
+        style({transform: 'translateX(-100%) scaleY(1)'}),
+        animate('.3s ease-in', style({transform: 'translateX(0) scaleY(0)'})),
       ]),
     ]),
     CustomAnimation.animateScaleY,
@@ -46,8 +45,12 @@ export class MspEdgeComponent {
 
   icons = Icons;
   isPanelOpen = false;
+  isPanelOpenAnimate = new BehaviorSubject(false);
 
   path: { cost, description }[];
+
+  positionLeftCenter = [new ConnectionPositionPair(
+    {originX: 'start', originY: 'center'}, {overlayX: 'start', overlayY: 'center'})];
 
   toggleSidePanel() {
     this.isPanelOpen = !this.isPanelOpen;
