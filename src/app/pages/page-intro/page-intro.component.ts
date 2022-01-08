8cc75ed242be3b92862abc0aa698eb1b242923c5
Pierre Roux <pierre.roux6@gmail.com>
2022-01-08 17:57:37 +0200

    Add footer

--- src/app/pages/page-intro/page-intro.component.ts
+++ src/app/pages/page-intro/page-intro.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy } from '@angular/core';
+import { filter, sampleTime, scan, skip, Subject, takeUntil, tap } from 'rxjs';
+import { WithDestroy } from '../../common/with-destroy';
+import { Router, Scroll } from '@angular/router';
 
 @Component({
   selector: 'cp-page-intro',
   templateUrl: './page-intro.component.html',
   styleUrls: ['./page-intro.component.scss']
 })
-export class PageIntroComponent {
+export class PageIntroComponent extends WithDestroy() implements OnDestroy {
 
   calcDv = {
     result: 0,
@@ -17,9 +20,66 @@ export class PageIntroComponent {
       this.calcDv.result = dv.toInt();
     }
   };
+  showSidebar = false;
+  showNavbar = true;
+  scrollEvent$ = new Subject<number>()
+  pageHeight = 1000;
 
-  constructor() {
-    this.calcDv.update();
+  log(a: any) {
+    console.log(a)
   }
 
+  constructor(router: Router,
+              self: ElementRef) {
+    super();
+
+    this.calcDv.update();
+
+    this.scrollEvent$
+      .pipe(
+        sampleTime(17),
+        scan((acc, value) => {
+          acc.show = value < acc.lastValue;
+          acc.lastValue = value;
+          return acc;
+        }, {lastValue: 0, show: true}),
+        skip(1),
+        tap(({show}) => {
+          this.showNavbar = show;
+          if (!show) {
+            this.showSidebar = false;
+          }
+        }),
+        takeUntil(this.destroy$))
+      .subscribe();
+
+    router.events
+      .pipe(
+        filter((e: any) => e instanceof Scroll),
+        filter((e: Scroll) => !!e.anchor),
+        tap((e: Scroll) => self.nativeElement
+          .querySelector(`#${e.anchor}`)
+          .scrollIntoView({behavior: 'smooth', block: 'start'})),
+        takeUntil(this.destroy$))
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    super.ngOnDestroy();
+    this.scrollEvent$.complete();
+  }
+
+  updateNavbar(event: Event & { target: HTMLElement }) {
+    this.scrollEvent$.next(event.target.scrollTop);
+  }
+
+  updateSidebar(event: PointerEvent & { path: HTMLElement[] }) {
+    if (!this.showSidebar) {
+      return;
+    }
+    let clickedOnSidebar = event.path.some(e => e.id === 'sidebar' || e.id === 'sidebar-button');
+    if (!clickedOnSidebar) {
+      this.showSidebar = false;
+    }
+  }
 }
