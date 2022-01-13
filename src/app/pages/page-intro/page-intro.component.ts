06c065f5c8c17b01abcaed8b33c9a4e383b4d230
Pierre Roux <pierre.roux6@gmail.com>
2022-01-13 20:25:29 +0200

    Add CommNet section

--- src/app/pages/page-intro/page-intro.component.ts
+++ src/app/pages/page-intro/page-intro.component.ts
@@ -1,16 +1,15 @@
-import { Component, ElementRef, OnDestroy } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 import { filter, sampleTime, scan, skip, Subject, takeUntil, tap } from 'rxjs';
 import { WithDestroy } from '../../common/with-destroy';
 import { Router, Scroll } from '@angular/router';
-import { ThemeService, ThemeTypeEnum } from '../../services/theme.service';
-import { Icons } from '../../common/domain/icons';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'cp-page-intro',
   templateUrl: './page-intro.component.html',
   styleUrls: ['./page-intro.component.scss']
 })
-export class PageIntroComponent extends WithDestroy() implements OnDestroy {
+export class PageIntroComponent extends WithDestroy() implements OnInit, OnDestroy {
 
   calcDv = {
     result: 0,
@@ -32,7 +31,7 @@ export class PageIntroComponent extends WithDestroy() implements OnDestroy {
   }
 
   constructor(router: Router,
-              self: ElementRef,
+              private self: ElementRef,
               private themeService: ThemeService) {
     super();
 
@@ -67,6 +66,15 @@ export class PageIntroComponent extends WithDestroy() implements OnDestroy {
       .subscribe();
   }
 
+  ngOnInit() {
+    (Array.from(this.self.nativeElement.querySelectorAll(`section[id]`)) as HTMLElement[])
+      .map(e => e.querySelector('h1, h2, h3, h4, h5, h6').querySelector('a'))
+      .forEach((e: any) => {
+        let cssEncapsulationId = (Array.from(e.attributes) as any).find((a: any) => a.name.includes('_ngcontent-')).name;
+        e.innerHTML = createLinkSvgIcon(cssEncapsulationId);
+      })
+  }
+
   ngOnDestroy() {
     super.ngOnDestroy();
     this.scrollEvent$.complete();
@@ -90,4 +98,8 @@ export class PageIntroComponent extends WithDestroy() implements OnDestroy {
     }
   }
 
+}
+
+function createLinkSvgIcon(cssEncapsulationId) {
+  return `<svg ${cssEncapsulationId} viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg>`;
 }
