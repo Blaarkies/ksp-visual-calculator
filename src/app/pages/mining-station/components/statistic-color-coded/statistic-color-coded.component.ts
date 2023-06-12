b817af74f37ec479e31fb3fdd1638605051968b6
Blaarkies <pierre.roux6@gmail.com>
2023-06-12 20:20:54 +0200

    fix isru mobile layout

--- src/app/pages/mining-station/components/statistic-color-coded/statistic-color-coded.component.ts
+++ src/app/pages/mining-station/components/statistic-color-coded/statistic-color-coded.component.ts
@@ -1,9 +1,17 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { OverlayModule } from '@angular/cdk/overlay';
 import { CommonModule } from '@angular/common';
-import { MouseHoverDirective } from '../../../../directives/mouse-hover.directive';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { BasicAnimations } from '../../../../animations/basic-animations';
-import { ConfigurableAnimations } from '../../../../animations/configurable-animations';
 import { Common } from '../../../../common/common';
+import { Icons } from '../../../../common/domain/icons';
+import { MouseHoverDirective } from '../../../../directives/mouse-hover.directive';
 
 @Component({
   selector: 'cp-statistic-color-coded',
@@ -11,24 +19,40 @@ import { Common } from '../../../../common/common';
   imports: [
     CommonModule,
     MouseHoverDirective,
+    OverlayModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   templateUrl: './statistic-color-coded.component.html',
   styleUrls: ['./statistic-color-coded.component.scss'],
   animations: [
-    BasicAnimations.expandX,
-    ConfigurableAnimations.openCloseX(40),
+    BasicAnimations.width,
   ],
 })
 export class StatisticColorCodedComponent implements OnChanges {
 
+  @Input() label: string;
   @Input() low: number = 0;
   @Input() high: number = 0;
 
   @Input() reverseSentiment: boolean = false;
 
+  positionConnection = Common.createConnectionPair('↘');
   excess: number;
   isGood: boolean;
-  showDetails = false;
+  icons = Icons;
+
+  isOverlayOpen = false;
+  isAnimating = false;
+  isHoveringOverlay = false;
+
+  set isOpen(value: boolean) {
+    if (value !== this.isOverlayOpen) {
+      this.isAnimating = true;
+    }
+
+    this.isOverlayOpen = value;
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     let difference = this.high - this.low;
@@ -40,8 +64,29 @@ export class StatisticColorCodedComponent implements OnChanges {
       : excess >= 0;
   }
 
-  updateHover(isHover: boolean) {
-    this.showDetails = isHover;
+  updateHoverOrigin(isHover: boolean) {
+    if (this.isAnimating) {
+      return;
+    }
+    if (!isHover && this.isHoveringOverlay) {
+      return;
+    }
+
+    this.isOpen = isHover;
   }
 
+  updateHoverOverlay(isHover: boolean) {
+    this.isHoveringOverlay = isHover;
+    if (this.isAnimating) {
+      return;
+    }
+    this.isOpen = isHover;
+  }
+
+  clickedToClose() {
+    if (this.isAnimating) {
+      return;
+    }
+    this.isOpen = false;
+  }
 }
